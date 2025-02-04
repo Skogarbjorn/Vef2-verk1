977bdfc18e2df8123b190a0586dfd633fad3d70f
Skogarbjorn <ragnarbjorn88@gmail.com>
2025-02-04 17:02:30 -0100

    Added main.js as module

--- src/backend/generateHTML.js
+++ src/backend/generateHTML.js
@@ -81,6 +81,7 @@ function generateIndexHTML(data) {
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script src="src/frontend/main.js" type="module"></script>
 		<title>categories</title>
 		<link rel="stylesheet" href="./styles.css">
 	</head>
